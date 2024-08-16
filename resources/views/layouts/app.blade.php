a166286360c59a27b7e8efc15735447c96a53aec
ugyawali <ugyawali@myune.edu.au>
2024-08-17 00:00:46 +1000

    Implemented the admin middleware

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,14 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -30,22 +23,13 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav nav-underline" id="myNav" >
-                        <!-- Menu Items -->
-                        <!-- Nav tabs -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="/posts/create">Create</a>
-                          </li>
-                    </ul>
-
+                    <ul class="navbar-nav me-auto">
 
                     </ul>
 
@@ -94,11 +78,8 @@
                     @yield('content')
                 </div>
             </div>
-    </div>
-    </div>
 
-    </main>
+        </main>
     </div>
 </body>
-
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,15 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\PhotoController;
 
 Route::get('/', function () {
-    return (new PostController)->index();
-});
+    if (Auth::check()) {
+        return redirect()->route('dashboard');
+    } else {
+        return view('auth.login');
+    }
+})->name('home');
+Route::get('/dashboard', function () {
+    return view('home');
+})->name('dashboard')->middleware('auth');
 
 Auth::routes();
 Route::get('posts', [PostController::class, 'index'])->name('posts.index');
@@ -15,8 +22,11 @@ Route::get('posts/create', [PostController::class, 'create'])->name('post.create
 Route::post('posts', [PostController::class, 'store'])->name('post.store');
 Route::get('posts/{id}', [PostController::class,'show'])->name('post.show');
 Route::get('posts/{id}/edit', [PostController::class, 'edit'])->name('post.edit');
-
 Route::post('posts/{id}', [PostController::class, 'update'])->name('post.update');
 Route::delete('posts/{id}', [PostController::class, 'destroy'])->name('post.delete');
 
 Route::resource('photos',PhotoController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
